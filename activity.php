c905887d9baeb2d87653cb6bf118ef1a614b5ed7
Alfian Azis <alfian.azis95@gmail.com>
2023-09-29 13:49:37 +0700

    add hello world inactivity

--- activity.php
+++ activity.php
@@ -1,3 +1,6 @@
 <?php
 $qinsertact	= mysqli_query ($koneksi,"insert into activity (iduser, nik, activity, date, ip) values ($iduseract, '$nikpa', '$activity', now(), '".$_SERVER['REMOTE_ADDR']."')");
+
+echo "Hello World";
+
 ?>
