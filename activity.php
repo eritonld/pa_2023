80b857fd42ccc9245509a9bcec8f9bf8787ab30c
Eriton Latti Dewa <eriton.dewa@Gamadomain.local>
2023-09-29 15:43:29 +0700

    test

--- activity.php
+++ activity.php
@@ -1,3 +1,4 @@
 <?php
 $qinsertact	= mysqli_query ($koneksi,"insert into activity (iduser, nik, activity, date, ip) values ($iduseract, '$nikpa', '$activity', now(), '".$_SERVER['REMOTE_ADDR']."')");
+//test
 ?>
